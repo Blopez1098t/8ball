083d8c0d76c024fce67020ba0ed95ff702c9ce22
Bryan <125blopez@frhsd.com>
2023-11-16 10:10:13 -0500

    work

--- js/8ball.js
+++ js/8ball.js
@@ -14,36 +14,36 @@ let eightBall = ""
 
 switch (randomNumber){
     case '0':
-    console.log('It is certain');
+        eightball=('It is certain');
     break;
 
     case '1':
-    console.log('It is decidedly so');
+        eightball=('It is decidedly so');
     break;
 
     case '2':
-    console.log('Reply hazy try again');
+        eightball=('Reply hazy try again');
     break;
 
     case '3':
-    console.log('Cannot predict now');
+        eightball=('Cannot predict now');
     break;
 
     case '4':
-    console.log('Do not count on it');
+        eightball=('Do not count on it');
     break;
 
     case '5':
-    console.log('My sources say no');
+        eightball=('My sources say no');
     break;
     
     case '6':
-    console.log('Outlook not so good');
+        eightball=('Outlook not so good');
     break;
 
     case '7':
-    console.log('Signs point to yes');
+    eightball=('Signs point to yes');
     break;
 }
 
-console.log("Magic Eightball: ${eightball}");
+console.log(eightball);
